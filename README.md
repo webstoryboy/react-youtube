12513284435a17652daf103c53f1400919459144
웹스토리보이 <webstupids@gmail.com>
2023-10-17 14:20:42 +0900

    페이지 만들기

--- README.md
+++ README.md
@@ -10,4 +10,5 @@
 ## 사용 스택
 
 ## 라이브러리 설치
-sass를 설치 : `npm install sass`
+sass 설치 : `npm install sass`
+react-router-dom 설치 : `npm install react-router-dom`
--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Youtuber from './pages/Youtuber'
+import Not from './pages/Not'
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/today' element={<Today />} />
+                <Route path='/youtuber' element={<Youtuber />} />
+                <Route path='*' element={<Not />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
