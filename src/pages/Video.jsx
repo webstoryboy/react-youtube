6403bfc82888df09a7b2cece20f140011b7f6629
웹스토리보이 <webstupids@gmail.com>
2023-10-24 10:19:00 +0900

    2023.10.24

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchFromAPI } from '../utils/api';
+import ReactPlayer from 'react-player';
 
 const Video = () => {
     const { videoId } = useParams();
@@ -8,15 +9,41 @@ const Video = () => {
 
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${videoId}`)
-            .then((data) => console.log(data));
+            .then((data) => {
+                setVideoDetail(data.items[0]);
+                console.log(data);
+            });
     }, [videoId]);
 
     return (
-        <div>
-            <span>비디오 제목</span>
-            <span>비디오 좋아요</span>
-            <span>비디오 뷰</span>
-        </div>
+        <section id='videoViewPage'>
+            {videoDetail && (
+                <div className='video__view'>
+                    <div className='video__play'>
+                        <ReactPlayer 
+                            playing={true}
+                            url={`https://www.youtube.com/watch?v=${videoId}`}
+                            width='100%'
+                            height='100%'
+                            style={{position: 'absolute', top: 0, left: 0}}
+                        />
+                    </div>
+                    <div className='video__info'>
+                        <h2 className='video__title'>
+                            {videoDetail.snippet.title}
+                        </h2>
+                        <div className='video__channel'>
+                            <div className='id'></div>
+                            <div className='count'>
+                                <span className='view'></span>
+                                <span className='like'></span>
+                                <span className='comment'></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </section>
     )
 }
 
